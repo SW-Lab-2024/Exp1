8ba7e28b26b0c0eb43b2fcf21c53005719d24316
amiiirdara <daraieamirmahdi@gmail.com>
2024-07-13 22:18:25 +0330

    reformatting the dummy.py

--- dummy.py
+++ dummy.py
@@ -1,5 +1,7 @@
-print ("Hi lets create dummy file")
-print("Thiss is saftware honework")
-print("Hello world!")
 def show_greating_message():
-    print("Welcome!glad to see you again!")
+    print("Welcome! glad to see you again!")
+    print("Hi let's create dummy file")
+    print("Thiss is saftware honework")
+    print("Hello world!")
+
+show_greating_message()

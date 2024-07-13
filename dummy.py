4617877649fa0819492677e00fd5f1a87fcf9ec0
Helia Akhtarkavian <heliakaviansharif@gmail.com>
2024-07-13 21:13:34 +0330

    remove odd space from dummy.py

--- dummy.py
+++ dummy.py
@@ -1,5 +1,8 @@
-print ("Hi lets create dummy file")
-print("Thiss is saftware   honework")
-print("Hello world!")
 def show_greating_message():
-    print("Welcome!glad to see you again!")
+
+    print("Welcome! glad to see you again!")
+    print("Hi lets create dummy file")
+    print("Thiss is saftware honework")
+    print("Hello world!")
+
+show_greating_message()

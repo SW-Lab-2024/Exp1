1b72543e1c282b44f8aeb8c48afcbc749b945eae
Helia Akhtarkavian <heliakaviansharif@gmail.com>
2024-07-13 22:17:13 +0330

    resolve incorrect dictations of dummy.py

--- dummy.py
+++ dummy.py
@@ -1,5 +1,5 @@
-print ("Hi lets create dummy file")
-print("Thiss is saftware honework")
+print ("Hi let's create dummy file")
+print("This is software homework")
 print("Hello world!")
 def show_greating_message():
     print("Welcome!glad to see you again!")

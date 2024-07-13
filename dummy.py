b96985a67225798ddd964393d5fc95ffcc44b0fc
Helia Akhtarkavian <heliakaviansharif@gmail.com>
2024-07-13 21:18:17 +0330

    resolve merge conflicts of dummy.py

--- dummy.py
+++ dummy.py
@@ -1,8 +1,5 @@
+print ("Hi lets create dummy file")
+print("Thiss is saftware honework")
+print("Hello world!")
 def show_greating_message():
-
-    print("Welcome! glad to see you again!")
-    print("Hi lets create dummy file")
-    print("Thiss is saftware honework")
-    print("Hello world!")
-
-show_greating_message()
+    print("Welcome!glad to see you again!")
